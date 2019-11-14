d8fdd5e991765fe16eb57989229c486c858f584c
zjw <jiangweizhu713@163.com>
2019-11-15 01:38:46 +0800

    home header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>home</div>
+  <div>
+    <home-header></home-header>
+  </div>
 </template>
+
+<script>
+// 引入组件
+import HomeHeader from './componets/Header'
+export default {
+  name: 'Home',
+  // 挂载子组件
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
